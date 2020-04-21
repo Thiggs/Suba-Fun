54c0ec1156ae2ca730b0953b7011f5805407febf
tyler <realpsych22@gmail.com>
2020-04-21 12:59:18 -0500

    Add type to data structures for unknown/learning/known

--- app/datasets/Data.js
+++ app/datasets/Data.js
@@ -1,38 +1,47 @@
 const data = [{
     problem: 1,
     answer:1,
+    type: "unknown",
     buck: 1
 },{
     problem: 2,
     answer:2,
+    type: "unknown",
     buck: 1
 },{
     problem: 3,
     answer:3,
+    type: "unknown",
     buck: 1
 },{
     problem: 4,
     answer:4,
+    type: "unknown",
     buck: 1
 },{
     problem: 5,
     answer:5,
+    type: "unknown",
     buck: 1
 },{
     problem: 6,
     answer:6,
+    type: "unknown",
     buck: 1
 },{
     problem: 7,
     answer:7,
+    type: "unknown",
     buck: 1
 },{
     problem: 8,
     answer:8,
+    type: "unknown",
     buck: 1
 },{
     problem: 9,
     answer:9,
+    type: "unknown",
     buck: 1
 }];
 
