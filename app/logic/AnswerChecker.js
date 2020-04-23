2aa069d16a7b357fa77ec0e1e9bf13d3138f9a34
tyler <realpsych22@gmail.com>
2020-04-22 22:55:45 -0500

    just added some comments to AnswerChecker regarding last commit

--- app/logic/AnswerChecker.js
+++ app/logic/AnswerChecker.js
@@ -5,23 +5,27 @@ let questionHolder;
 
 function answerChecker(userAnswer, currentState){
     var newState = currentState;
+    //Provides a distractor if previous answer was incorrect
     if (currentState.distractor == 2){
         newState.question=0;
         newState.prompt= "Right! Let's try again to be sure.";
         newState.choices=["Tap to Continue"];
         newState.distractor= 1;
     }
+    //re-presents the original problem after a distractor
     else if (currentState.distractor == 1){
         newState.question=questionHolder;
         newState.choices=choiceSetHolder;
         newState.distractor=0;
         newState.prompt=null;
     }
+    //only if the answer is correct do we move on to the next question in the trial
     else if (userAnswer == currentState.question){
       var nextQuestion=questionMaker();
       newState.question=nextQuestion.questions;
       newState.choices=nextQuestion.choiceSet;
     }
+    //if incorrect answer is given, we need to provide it errorlessly, then provide a distractor, then provide it again
     else {
     choiceSetHolder=currentState.choices;
     questionHolder=currentState.question;
